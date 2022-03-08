a91e3169ed58ca61229159f894fe1df5b86a47f9
Rakic_Jugo <rakic.nemanja@hak-liezen.at>
2022-03-08 16:01:20 +0100

    Initial commit

---
+++ Delegate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo input;
+            ConsoleKeyInfo s;
+            
+           
+            do
+            {
+                Console.WriteLine("Bitte geben sie eiine zahl ein");
+                int zahl = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Bitte eine zweite");
+                int zahl2 = Convert.ToInt32(Console.ReadLine());
+
+                
+                Console.WriteLine("S oder a");
+                s = Console.ReadKey();
+              
+                if (s.Key==ConsoleKey.S)
+                {
+                    
+                    Console.Clear();
+                    int ergebnis = zahl - zahl2;
+                    Console.WriteLine("Das Ergebnis lautet: "+ergebnis);
+                }
+                else if (s.Key == ConsoleKey.A)
+                {
+                    Console.Clear();
+                    int ergebnis2 = zahl + zahl2;
+                    Console.WriteLine("Das Ergebnis lautet: " + ergebnis2);
+                }
+
+
+
+
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("um das programm zuschließen bitte F12");
+                input = Console.ReadKey();
+            } while (input.Key != ConsoleKey.F12);
+        }
+    }
+}
